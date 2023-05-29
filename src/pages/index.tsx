c96f8ac218a8e77a5f16c621b4d7cf2e2b1e9a2e
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-29 19:48:17 -0300

    add: carousel effect with keen-slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,48 @@
-// import { Inter } from "next/font/google";
-
 import { HomeContainer, Product } from "@/styles/pages/home";
 import Image from "next/image";
 import camiseta1 from "../assets/camisetas/1.png";
 import camiseta2 from "../assets/camisetas/2.png";
-// import camiseta3 from "../assets/camisetas/3.png";
+import camiseta3 from "../assets/camisetas/3.png";
+
+import { useKeenSlider } from "keen-slider/react";
+
+import "keen-slider/keen-slider.min.css";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
         <footer>
           <strong>Camiseta X</strong>
           <span>R$ 28,00</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} />
         <footer>
-          <strong>Camiseta X</strong>
+          <strong>Camiseta 2X</strong>
+          <span>R$ 28,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 3X</strong>
+          <span>R$ 28,00</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+        <footer>
+          <strong>Camiseta 3X</strong>
           <span>R$ 28,00</span>
         </footer>
       </Product>
--- src/styles/pages/home.ts
+++ src/styles/pages/home.ts
@@ -2,7 +2,7 @@ import { styled } from "..";
 
 export const HomeContainer = styled("main", {
   display: "flex",
-  gap: "3rem",
+  // gap: "3rem",
   width: "100%",
   maxWidth: "calc(100vw - ((100vw - 1180px)/2))",
   marginLeft: "auto",
@@ -12,13 +12,15 @@ export const HomeContainer = styled("main", {
 export const Product = styled("a", {
   background: "linear-gradient(100deg, #1ea483 0%, #7465d4 100%)",
   borderRadius: 8,
-  padding: "0.25rem",
+  // padding: "0.25rem",
   cursor: "pointer",
   position: "relative",
 
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
+
+  minWidth: "540px",
 
   img: {
     objectFit: "cover",
