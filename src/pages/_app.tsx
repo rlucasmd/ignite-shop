dba1696796a0ab4e4e4c63a1c81bee60b27954a6
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-05-28 21:41:05 -0300

    add: header logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import logoImage from "../assets/logo.svg";
+// import Image from "next/image";
+import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImage.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
