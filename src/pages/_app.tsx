c8da61807f7b6b52a84d298456cb83386e226398
Ranieri Lucas <ranieri.dantas.122@ufrn.edu.br>
2023-06-01 20:59:53 -0300

    add: cart button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,8 @@ import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
 import logoImage from "../assets/logo.svg";
 import Image from "next/image";
-import { Container, Header } from "@/styles/pages/app";
+import { CartButton, Container, Header } from "@/styles/pages/app";
+import { Handbag } from "phosphor-react";
 
 globalStyles();
 
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Container>
       <Header>
         <Image src={logoImage} alt="" />
+        <CartButton>
+          <Handbag size={24} color="#8D8D99" weight="bold" />
+        </CartButton>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -12,4 +12,19 @@ export const Header = styled("header", {
   width: "100%",
   maxWidth: "1180px",
   margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+export const CartButton = styled("button", {
+  cursor: "pointer",
+  display: "flex",
+  background: "#202024",
+  padding: 12,
+  border: "none",
+  borderRadius: 8,
+  width: 48,
+  height: 48,
+  justifyContent: "center",
+  alignItems: "center",
 });
